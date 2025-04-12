9fd570e3f32ec3e3864ed98cad47443005f14913
MikhailSporyshev <mikhailsporyshev@gmail.com>
2025-04-12 22:35:45 +0800

    updated route, handler, test case for dignup

--- auth-service/tests/api/helpers.rs
+++ auth-service/tests/api/helpers.rs
@@ -1,4 +1,5 @@
 use auth_service::Application;
+use uuid::Uuid;
 
 pub struct TestApp {
     pub address: String,
@@ -38,9 +39,13 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
-    pub async fn post_signup(&self) -> reqwest::Response {
+    pub async fn post_signup<Body>(&self, body: &Body) -> reqwest::Response
+    where
+        Body: serde::Serialize,
+    {
         self.http_client
             .post(&format!("{}/signup", &self.address))
+            .json(body)
             .send()
             .await
             .expect("Failed to execute request.")
@@ -78,4 +83,8 @@ impl TestApp {
             .expect("Failed to execute request.")
     }
 
+}
+
+pub fn get_random_email() -> String {
+    format!("{}@example.com", Uuid::new_v4())
 }
