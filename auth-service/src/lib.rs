6bab255188c3734f871960d921d8334b9c98019f
MikhailSporyshev <mikhailsporyshev@gmail.com>
2025-04-12 22:09:00 +0800

    moved handlers into module

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -4,6 +4,8 @@ use axum::{http::StatusCode, response::IntoResponse, routing::{get, post}, serve
 use tower_http::services::ServeDir;
 
 pub mod routes;
+use routes::{signup, login, logout, verify_2fa, verify_token};
+
 
 // This struct encapsulates our application-related logic.
 pub struct Application {
@@ -42,20 +44,4 @@ impl Application {
         println!("listening on {}", &self.address);
         self.server.await
     }
-}
-
-async fn signup() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn login() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn logout() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn verify_2fa() -> impl IntoResponse {
-    StatusCode::OK.into_response()
-}
-async fn verify_token() -> impl IntoResponse {
-    StatusCode::OK.into_response()
 }
